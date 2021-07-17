b3cd23b7ebed2958640ed04666ed7e5052f68d58
XojeDesktop <dawxoje@gmail.com>
2021-07-17 18:03:24 +0200

    Config port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const { env } = require("process");
 const app = express();
-const port = 3000;
+const port = 3000 || env.PORT;
 app.use(express.static("public"));
 app.get("/", (req, res) => {
 	res.sendFile("index.html");
